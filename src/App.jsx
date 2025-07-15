b0debec1c0a031570449eaeafdd51cb336c41e50
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-15 17:42:27 +0200

    feat(navigation): create navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./App.css";
+import { Navigation } from "./components/Navigation/Navigation";
+import { Tabs } from "./components/Navigation/Tabs";
+
+const INITIAL_TAB = Object.keys(Tabs)[0];
 
 export const App = () => {
-    return (
-        <div className="app-container">
-            <h1 className="app-title">♻Mi lista de tareas</h1>
-        </div>
-    );
+	const [activeTab, setActiveTab] = useState(INITIAL_TAB);
+
+	return (
+		<div className="app-container">
+			<Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
+			<h1 className="app-title">♻Mi lista de tareas</h1>
+
+			<p>Pestaña activa {activeTab}</p>
+		</div>
+	);
 };
