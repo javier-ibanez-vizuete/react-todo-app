be33e9b31717e9abedf218aad26985d5a8a9776c
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-07-15 18:37:43 +0200

    feat(TodoForm): create form for new todo

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,3 +1,48 @@
+import { useState } from "react";
+import './TodoForm.css'
+
+const INITIAL_FORM_STATE = { todoText: "" };
+
 export const TodoForm = () => {
-	return <div>FORMULARIO</div>;
+	const [form, setForm] = useState(INITIAL_FORM_STATE);
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+
+		if (!form.todoText) console.log("Rellena el campo Texto!!!");
+
+		const newTodo = {
+			text: form.todoText,
+			completed: false,
+			done: false,
+			favorite: false,
+			id: Date.now(),
+		};
+
+		setForm(INITIAL_FORM_STATE);
+	};
+
+	const onInputChange = ({ target: { name, value } }) => setForm((prev) => ({ ...prev, [name]: value }));
+
+	return (
+		<>
+			<h2 className="form-title">✍🏿 Crear nueva tarea</h2>
+
+			<div className="todo-form-container">
+				<form action="#" className="todo-form" onSubmit={onFormSubmit}>
+					<input
+						type="text"
+						className="todo-input"
+						name="todoText"
+						value={form.todoText}
+						placeholder="Introduce una tarea"
+						onChange={onInputChange}
+					/>
+					<button className="add-btn" type="submit">
+						Añadir
+					</button>
+				</form>
+			</div>
+		</>
+	);
 };
